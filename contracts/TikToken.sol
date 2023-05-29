ff65501a19df4898cf1d59a8fd8e30c4dce1fc71
technicallyweb3 <mancinoweb3@gmail.com>
2023-05-29 13:10:52 -0600

    lock token transfer from owner until first halving

--- contracts/TikToken.sol
+++ contracts/TikToken.sol
@@ -35,6 +35,8 @@ contract TikToken is ERC20, Ownable {
 
     // Mapping to keep track of each unique TikTok user ID that has minted tokens
     mapping(uint256 => bool) private _minted;
+    // Mapping to associate user addresses with their IDs
+    mapping(address => uint256[]) private _userIDs;
 
     // Contract Events
     event Minted(address account, uint256 amount, uint256 tiktokId, uint256 followers);
@@ -46,6 +48,17 @@ contract TikToken is ERC20, Ownable {
     constructor() ERC20("TikToken", "TIK") {
         uint256 initialMintAmount = _maxSupply - _initialSupply;
         _mint(msg.sender, initialMintAmount);
+    }
+
+    // Functions prevent Owner from dumping tokens before first halving
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(_halvingCount > 1 || msg.sender != owner(), "Owner transfer locked until first halving");
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(_halvingCount > 1 || msg.sender != owner(), "Owner transfer locked until first halving");
+        return super.transferFrom(sender, recipient, amount);
     }
 
     function calculateRewards(uint256 followers) private view returns (uint256) {
@@ -71,15 +84,14 @@ contract TikToken is ERC20, Ownable {
 
     // Mint function allows the owner of the contract to mint tokens this is a owner function and this gives rise to a potential abuse of the function
     // This type of control means you must have alot of trust in the project and contract's owner espescially since there are no built-in follower limits
+    // Fortunately there's a public getter function to audit the minting so it's associated with a user ID and anyone can check the minting.
     // It gives out a large amount of tokens to early adopters and gradually reduces the reward as more tokens are minted based on an agressive 1/10th halving policy
     // Each user can earn tokens based on the number of their followers and how many halving cycles have happened
     function mint(address account, uint256 followers, uint256 id) public onlyOwner{
 
         require(_remainingSupply > 0, "No more tokens to mint"); //Ensures supply exists
-        //Skip this iteration if user has already minted tokens or does not have enough followers
-            if(_minted[id] || (followers < _followerSet && !_allUsersEarn)){
-                return;
-            }
+        require(!_minted[id], "User has already minted");
+        require(followers > _followerSet || _allUsersEarn, "Not enough followers to mint");
 
         uint256 amountToMint = calculateRewards(followers);
 
@@ -91,7 +103,10 @@ contract TikToken is ERC20, Ownable {
         //mint the tokens, adjust remaining supply and log the user id
         _mint(account, amountToMint);
         _remainingSupply -= amountToMint;
+        // Flag user ID as minted to prevent multiple minting
         _minted[id] = true;
+        // Add the ID to the user's list of IDs
+        _userIDs[account].push(id);
         _userCounter++;
         emit Minted(account, amountToMint, id, followers);
 
@@ -128,7 +143,7 @@ contract TikToken is ERC20, Ownable {
         }
     }
 
-    // Getter functions to view the remaining supply of tokens, the current reward, the user's minted status, and the number of halvings
+    // Getter functions to view the remaining supply of tokens, the current reward, the user's minted status, and the number of halvings, the IDs associated with a user's address.
     function remainingSupply() external view returns (uint256) {
         return _remainingSupply; //amount of TikTokens remaining to be minted
     }
@@ -151,7 +166,11 @@ contract TikToken is ERC20, Ownable {
 
     function getUserCounter() external view returns (uint256) {
         return _userCounter;
-    }    
+    }
+
+    function getUserIDs(address account) external view returns (uint256[] memory) {
+        return _userIDs[account];
+    }
     
     // Unacceptable! This gives the contract owner way too much unilateral control! This must be done as
     // Governance using the community and only after the 2nd halving to ensure fair distribution before
